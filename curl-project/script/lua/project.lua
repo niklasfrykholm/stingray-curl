3df0f61b3f0e5f448b59acbabfcdc05407ec31be
Niklas Frykholm <niklas.frykholm@autodesk.com>
2015-11-20 16:45:23 +0100

    Downloading and print HTML.

--- curl-project/script/lua/project.lua
+++ curl-project/script/lua/project.lua
@@ -17,7 +17,7 @@ local curl = require 'script/lua/luajit-curl'
 
 Http = Http or {}
 
-function Http.get(url)
+Http.get = function(url)
 	local ch = curl.curl_easy_init()
 	curl.curl_easy_setopt(ch, curl.CURLOPT_URL, url)
 	curl.curl_easy_setopt(ch, curl.CURLOPT_FOLLOWLOCATION, 1)
@@ -34,16 +34,40 @@ function Http.get(url)
 	local fptr = ffi.cast("size_t (*)(char *, size_t, size_t, void *)", cb)
 	curl.curl_easy_setopt(ch, curl.CURLOPT_WRITEFUNCTION, fptr)
 	local result = curl.curl_easy_perform(ch)
-	print("CURL-RESULT: ", result)
-	if result ~= curl.CURLE_OK then
-		print("CURL-FAILURE:", ffi.string(curl.curl_easy_strerror(result)))
-	else
-		print("CURL-SUCCESS:", data)
-	end
 	curl.curl_easy_cleanup(ch)
+	if result ~= curl.CURLE_OK then
+		return nil, ffi.string(curl.curl_easy_strerror(result))
+	else
+		return data
+	end
 end
 
-Http.get("http://www.example.com")
+SimpleGui = SimpleGui or {}
+
+SimpleGui.font = 'core/performance_hud/debug'
+SimpleGui.font_material = 'core/performance_hud/debug'
+SimpleGui.font_size = 20
+
+SimpleGui.init = function (world)
+	local state = {}
+	state.gui = stingray.World.create_screen_gui(world or stingray.Application.main_world(), "immediate")
+	local w,h = stingray.Application.back_buffer_size()
+	state.size = {width = w, height = h}
+	state.x = 50
+	state.y = 50
+	return state
+end
+
+SimpleGui.draw = function (state, text)
+	local pos = stingray.Vector2(state.x, state.size.height - state.y)
+	stingray.Gui.text(state.gui, text, SimpleGui.font, SimpleGui.font_size, SimpleGui.font_material, pos, stingray.Color(255,255,255))
+	state.y = state.y + 20
+end
+
+SimpleGui.button = function (state, text)
+	SimpleGui.draw(state, text)
+	return true
+end
 
 -- Can provide a config for the basic project, or it will use a default if not.
 local SimpleProject = require 'core/appkit/lua/simple_project'
@@ -66,8 +90,22 @@ end
 function Project.on_level_shutdown_post_flow()
 end
 
+local gui_state = nil
+
 -- Optional function called by SimpleProject after world update (we will probably want to split to pre/post appkit calls)
 function Project.update(dt)
+	-- Http.get("http://www.example.com")
+	gui_state = gui_state or SimpleGui.init()
+	local gs = gui_state
+	gs.x, gs.y = 50, 50
+	local data = ""
+	if SimpleGui.button(gs, "GET http://www.example.com") then
+		data = Http.get("http://www.example.com")
+	end
+	SimpleGui.draw(gs, "")
+	for s in string.gmatch(data, ".-\r?\n") do
+		SimpleGui.draw(gs, s)
+	end
 end
 
 -- Optional function called by SimpleProject *before* appkit/world render
