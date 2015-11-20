668533f85d6ece22fd8f18aa438b70e83ee188d0
Niklas Frykholm <niklas.frykholm@autodesk.com>
2015-11-20 10:57:55 +0100

    Basic curl binding.

--- curl_project/script/lua/project.lua
+++ curl_project/script/lua/project.lua
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------------------
--- This implementation uses the default SimpleProject and the Project extensions are 
+-- This implementation uses the default SimpleProject and the Project extensions are
 -- used to extend the SimpleProject behavior.
 
 -- This is the global table name used by Appkit Basic project to extend behavior
@@ -10,6 +10,21 @@ require 'script/lua/flow_callbacks'
 Project.level_names = {
 	empty = "content/levels/empty"
 }
+
+-- Load curl
+local ffi = require 'ffi'
+ffi.cdef [[
+	typedef enum {
+		CURLOPT_URL = 10000 + 2,
+	} CURLoption;
+	void *curl_easy_init();
+	int curl_easy_setopt(void *curl, int option, ...);
+	int curl_easy_perform(void *curl);
+	void curl_easy_cleanup(void *curl);
+]]
+local libcurl = ffi.load [[C:\Work\curl\bin\libcurl.dll]]
+local curl = libcurl.curl_easy_init()
+libcurl.curl_easy_setopt(curl, libcurl.CURLOPT_URL, "http://example.com")
 
 -- Can provide a config for the basic project, or it will use a default if not.
 local SimpleProject = require 'core/appkit/lua/simple_project'
@@ -22,18 +37,19 @@ SimpleProject.config = {
 	exit_standalone_with_esc_key = true
 }
 
--- Optional function by SimpleProject after level, world and player is loaded 
+-- Optional function by SimpleProject after level, world and player is loaded
 -- but before lua trigger level loaded node is activated.
 function Project.on_level_load_pre_flow()
 end
 
--- Optional function by SimpleProject after loading of level, world and player and 
+-- Optional function by SimpleProject after loading of level, world and player and
 -- triggering of the level loaded flow node.
 function Project.on_level_shutdown_post_flow()
 end
 
 -- Optional function called by SimpleProject after world update (we will probably want to split to pre/post appkit calls)
 function Project.update(dt)
+
 end
 
 -- Optional function called by SimpleProject *before* appkit/world render
